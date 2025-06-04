77884f4434bdb61ca99c96d6f7bdbda0d973e126
Charles Pickering <me@charlespick.xyz>
2025-06-03 20:49:43 -0700

    Add error handling to Set-NBConfig function for missing API key

--- Private/Config.ps1
+++ Private/Config.ps1
@@ -10,6 +10,9 @@ function Set-NBConfig {
         Write-Error "You must use an HTTPS URL for NetBox. Authentication will fail with non-HTTPS URLs."
         return
     }
+    if (-not $ApiToken) {
+        Write-Error "API token must be specified and cannot be empty."
+    }
     if ($BaseUrl -notmatch "/$") { $BaseUrl += "/" }
     $Script:NetBoxBaseUrl = $BaseUrl
     $Script:NetBoxApiToken = $ApiToken
