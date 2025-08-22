45acfbefd28b161d2c99ae636ed69e1f56a7ff30
charlespick <me@charlespick.xyz>
2025-08-22 07:54:12 -0700

    reflection time

---
+++ NetBoxPS.CodeGen/Program.cs
@@ -0,0 +1,31 @@
+﻿using NetBoxPS.CodeGen.Generators;
+using NetBoxPS.CodeGen.Reflection;
+using System;
+using System.Reflection;
+
+
+
+namespace NetBoxPS.CodeGen
+{
+    public static class Program
+    {
+        // ...
+
+        public static int Main(string[] args)
+        {
+            // Load the assembly from the file path provided in args[1]
+            var sdkAssembly = Assembly.LoadFrom(args[1]);
+            var reflector = new SdkReflector(sdkAssembly);
+            var endpoints = reflector.GetEndpoints();
+
+            var generator = new PowerShellFunctionGenerator(args[0]);
+            foreach (var ep in endpoints)
+            {
+                generator.GenerateFunction(ep);
+            }
+
+            Console.WriteLine("PowerShell function generation complete!");
+            return 0;
+        }
+    }
+}
